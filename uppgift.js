e607553ceb789b828b1adf2718fd42023e5d353c
ballesmak <112063916+ballesmak@users.noreply.github.com>
2022-09-22 22:08:13 +0200

    Add files via upload

---
+++ uppgift.js
@@ -0,0 +1,48 @@
+var ps = require("prompt-sync")
+var prompt = ps();
+/*
+let num = prompt("nummer")
+
+if( num > 0 ) {
+    console.log("hög")
+}if ( num < 0 ) {
+    console.log("låg")
+}if( num == 0 ) {
+  console.log("noll")
+}
+
+let djur = prompt("bokstav eller nummer?")
+
+if  (isNaN(djur)) {
+    console.log ("bokstav")
+}else {
+    console.log ("nummer")
+}
+
+
+
+let bog = prompt("vokal eller konsonant")
+
+    if (bog == "a","A")
+    console.log ("Vokal")
+    else if (bog == "e","E")
+    console.log ("Vokal")
+    else if (bog == "i","I")
+    console.log ("Vokal")
+    else if (bog == "o","O")
+    console.log ("Vokal",)
+    else if (bog == "u","U")
+    console.log ("Vokal")
+    else if (bog == "y","Y")
+    console.log ("Vokal")
+    else if (bog == "å","Å")
+    console.log ("Vokal")
+    else if (bog == "ä","Ä")
+    console.log ("Vokal")
+    else if (bog == "ö","Ö")
+    console.log ("Vokal")
+    else {
+    console.log ("konsonant")
+} 
+*/ 
+consol.log
